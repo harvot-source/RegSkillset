6cda5e2f4c9aec7011025005076d05edb67fcb6f
Jaykim Alivio <jaykimalivio21@gmail.com>
2023-08-08 15:19:16 +0800

    my activity

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const path = require("path")
+const app = express();
+const port = 10000;
+const dotenv = require("dotenv");
+const cookie_parser = require('cookie-parser');
+// const mysql = require("mysql2");
+
+dotenv.config({path:"./.env"});
+app.set("view engine", "hbs");
+app.use(express.static(path.join(__dirname, "./public")));
+
+app.use(express.urlencoded({extended:true})); // parse body url
+app.use(express.json()); // parse request 
+
+app.use(cookie_parser());
+
+//define routes imported from another file using the app.use 
+app.use("/", require("./routes/registerRoutes"));
+app.use("/auth", require("./routes/auth"));
+
+
+
+
+// app.get('/', (req,res)=>{
+//     res.send(`<html><body><h1>Registration Form</h1></body></html>`)
+// });
+
+app.listen(port, ()=>{
+    console.log(`Server has started!`);
+})
+.on("error", (err)=>{
+    console.error(`Server failed to start\n${err}`);
+});
---
+++ routes/auth.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const registrationController = require("../controller/authAccount");
+
+
+router.post("/register", registrationController.register);
+
+router.post("/login", registrationController.login);
+
+router.get("/updateform/:email", registrationController.updateform); // add colon (:) to get the value
+router.post("/updateuser", registrationController.updateuser);
+
+router.get("/accountRemove/:email", registrationController.accountRemove);
+router.post("/remove", registrationController.remove);
+
+router.get("/logout", registrationController.logout);
+
+router.get("/skillset/:email", registrationController.skillset);
+
+module.exports = router;
